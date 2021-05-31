eb2a187414c1a26c592396963c1897625c64eea3
first <harryjaiswal2077@gmail.com>
2021-05-31 10:21:20 +0545

    initial commit

---
+++ LabTwo.py
@@ -0,0 +1,8 @@
+'''
+Write a program that reads the length of the base and the height of a right-angled triangle and prints the area.
+ Every number is given on a separate line.
+'''
+num1 = int(input("enter the base of triangle"))
+num2 = int(input("enter the height of triangel"))
+area = num1 * num2
+print("The Area of right angled triangel:",area)
