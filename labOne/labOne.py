97836274517c2257e8fb064152f6ef38b9015464
first <harryjaiswal2077@gmail.com>
2021-06-10 14:12:01 +0545

    initial commit

--- labOne/labOne.py
+++ labOne/labOne.py
@@ -1,9 +1,7 @@
-'''
-Write a program that takes three numbers and prints their sum .
-Every number is given on a separate line.
-'''
-num1 =int(input("enter the first value"))
-num2 =int(input("enter the second value"))
-num3 =int(input("entter the third value"))
-sum = num1+num2+num3
-print(f"The of given three numbers is {sum}")
+for i in range(5):
+    if i==2:
+        print("two")
+        continue
+    print(i)
+
+print('a')
