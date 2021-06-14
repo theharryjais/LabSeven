923a8f1fda74a02c80a89ac21035f8552bbed0d0
first <harryjaiswal2077@gmail.com>
2021-06-14 09:57:08 +0545

    initial commit

---
+++ conditions/Conditions.py
@@ -0,0 +1,16 @@
+'''
+Price of a house is $1M.
+If buyer has good credit,
+they need to put down 10%
+otherwise
+they need to put down 20%.
+Print the down payment.
+'''
+price = 1000000
+has_good_Credit = False
+
+if has_good_Credit:
+    down_payment = 0.1 * price
+else:
+    down_payment = 0.2 * price
+print(f"Down payment:${down_payment}")
