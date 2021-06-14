bd5c44450cab313422b7ef7a66017fcea9a751b9
first <harryjaiswal2077@gmail.com>
2021-06-14 09:53:24 +0545

    initial commit

--- LabFive.py
+++ LabFive.py
@@ -4,3 +4,18 @@ The program should read three integers: the number of students in each of the th
 In the first test there are three groups. The first group has 20 students and thus needs 10 desks. The second group has 21 students, so they can get by with no fewer than 11 desks. 11 desks are also enough for the third group of 22 students. So, we need 32 desks in total.
 
 '''
+sectionA = int(input("enter the number of student "))
+sectionB=int(input("enter the number of students "))
+sectionC=int(input("enter the number of students"))
+a= sectionA//2
+if sectionA%2==1:
+    a=a+1
+b= sectionB//2
+if sectionB%2==1:
+    b=b+1
+c= sectionC//2
+if sectionC%2==1:
+    c=c+1
+print(f"the first group has {sectionA} number of students so they require {a} desk ")
+print(f"the first group has {sectionB} number of students so they require {b} desk ")
+print(f"the first group has {sectionC} number of students so they require {c} desk ")
---
+++ LabSix.py
@@ -0,0 +1,5 @@
+'''
+6. Solve each of the following problems using Python Scripts. Make sure you use appropriate variable names and comments. When there is a final answer have Python print it to the screen.
+A person’s body mass index (BMI) is defined as:a=
+BMI=mass in kg / (height in m)2.
+'''
